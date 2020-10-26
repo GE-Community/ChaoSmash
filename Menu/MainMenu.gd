303013e83232662ecd146b14cf9e8e01fe618969
AbhinavKDev <62714538+abhinav3967@users.noreply.github.com>
2020-10-26 14:41:30 +0530

    Added TagNames To The Main Menu (#8)

--- Menu/MainMenu.gd
+++ Menu/MainMenu.gd
@@ -6,12 +6,20 @@ var backgrounds : Array = ["res://Assets/Art/UI/Background/menu2.png",
 
 # Onready Variables
 onready var background_node : TextureRect = $Background
+onready var tagname_label : Label = $TagName
 
 func _ready():
 	randomize() #Using Randomize so that rand_range gives random results every time
 	var background : int = round(rand_range(0, backgrounds.size()-1)) # Getting a random index for background
 	var background_texture : Texture = load(backgrounds[background]) # Loading background texture
 	background_node.set_texture(background_texture) # Setting background
+	var tagname_file = File.new()
+	tagname_file.open("res://Menu/Tagname.json", File.READ)
+	var tempdata = parse_json(tagname_file.get_as_text())
+	var tagname_text = tempdata["tagnames"]
+	tagname_text.shuffle()
+	tagname_label.set_text(tagname_text[0])
+	$TagName/AnimationPlayer.play("Grow")
 
 func _on_PlayBtn_pressed():
 	SceneManager.goto("PlayMenu") # Changing scene to PlayMenu
